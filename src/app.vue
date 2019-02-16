194026570a4db5b7cba95fd22e1497cb90e70aec
DianaAdvani <dianacadvani@gmail.com>
2019-02-15 18:47:29 -0800

    Got vue working

---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }
+    ]
+  },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
-    }
-  }
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
 };
